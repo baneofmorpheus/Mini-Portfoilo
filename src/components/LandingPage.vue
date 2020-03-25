e5e0bd4bee5459e3bba615791581cc16da2ac3fe
baneofmorpheus <epicgenii18@gmail.com>
2020-03-25 13:18:43 +0100

    added link functionality for all links

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -3,7 +3,7 @@
     <b-row class="justify-content-center">
       <b-col cols="4" md="2">
         <div class="d-flex justify-content-center">
-          <img class="img-fluid rounded-circle bg-white " src="@/assets/img/1.png" alt="">
+          <img class="img-fluid rounded-circle bg-white " src="@/assets/img/huey.png" alt="">
         </div>
         
 
@@ -24,8 +24,8 @@
           </b-col>
          
           <b-col cols="12" class="mt-2">
-             <span><a href="#"> <i class="fab fa-linkedin-in"></i> </a></span>
-             <span class="ml-3"><a href="#"> <i class="fab fa-github"></i> </a></span>
+             <span><a href="https://www.linkedin.com/in/leon-chux/" target="_blank"> <i class="fab fa-linkedin-in"></i> </a></span>
+             <span class="ml-3"><a href="github.com/baneofmorpheus" target="_blank"> <i class="fab fa-github"></i> </a></span>
 
           </b-col>
 
@@ -35,7 +35,7 @@
 
       </b-col>
       <b-col cols="12" md="8" lg ="6"  class="mt-3 text-center">
-        <div :key="value.id" v-for='(value) in portfoilo_data' class="rounded-pill bg-dark pt-2 pb-2 portfoilo-pill mb-2 portfoilo-shadow" >
+        <div :key="value.id" @click="navigate(value.id)" v-for='(value) in portfoilo_data' class="rounded-pill bg-dark pt-2 pb-2 portfoilo-pill mb-2 portfoilo-shadow" >
             <p class="text-white m-0">{{value.name}}</p>
             <p class="text-white m-0"><small>{{value.description}}</small></p>
           </div>
@@ -57,10 +57,11 @@ export default {
       portfoilo_data:{
         one:{
           id:"one",
-          url:"http://www.arquilaxgroup.com/",
-          name:"Arquilax Group",
-          description:"Company profile site with logistics tracking system"
+          url:"https://www.valdezartsworld.com/",
+          name:"Valdez World",
+          description:"An art display site "
         },
+      
         two:{
           id:"two",
           url:"https://luxxeplace.com/",
@@ -116,9 +117,25 @@ export default {
           name:"True Blue Energy",
           description:"Company profile site"
         },
+          twelve:{
+          id:"twelve",
+          url:"http://www.arquilaxgroup.com/",
+          name:"Arquilax Group",
+          description:"Company profile site with logistics tracking system"
+        },
       }
     }
+  },
+  methods:{
+    navigate(id){
+      let url = this.portfoilo_data[id]['url']
+      window.open(url, '_blank');
+      console.log(url)
+
+
+    },
   }
+
 }
 </script>
 
@@ -142,6 +159,10 @@ export default {
 .portfoilo-shadow {
     -webkit-box-shadow: 0 1rem 3rem rgba(0, 0, 0, 0.4) !important;
     box-shadow: 0 1rem 3rem rgba(0, 0, 0, 0.4) !important;
+}
+
+i{
+  font-size:1.3rem;
 }
 
 </style>
